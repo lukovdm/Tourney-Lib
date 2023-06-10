913f988e32ed0be764695ce49f128cd5b75d76d2
Luko van der Maas <me@luko.dev>
2023-06-10 22:24:40 +0200

    initial flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A test project";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell
+          {
+            nativeBuildInputs = [ pkgs.rustup ];
+          };
+      });
+}
